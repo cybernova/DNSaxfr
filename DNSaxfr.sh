050dda18b925eca3f556651bb5bdc9d8d84ba530
Andrea Dari <andreadari91@gmail.com>
2015-09-01 16:57:33 +0200

    BIG update: added -r and -p options and more...
    Now this script is able to recursively tests every subdomain of a vulnerable domain, drawing all in a customizable tree (-r option).
    Proxychains' output now can be discarded using the -q option.
    If a domain is vulnerable "VULNERABLE" is colored in green and "NOT VULNERABLE" in red.

--- DNSaxfr.sh
+++ DNSaxfr.sh
@@ -23,9 +23,10 @@
 filter()
 {
 	#Only the characters found in $IFS are recognized as word delimiters.
-	while read SITE
+	while read DOMAIN
 	do
-		digSite $SITE
+		DOMAINLVL=$(echo $DOMAIN | sed -e 's/\.$//' | awk -F . '{ print NF }')
+		digSite $DOMAIN
 	done
 }
 
@@ -33,9 +34,10 @@ alexaTop500()
 {
 	for VAL in $(seq 0 19)
 	do
-		for SITE in $(wget -qO- "http://www.alexa.com/topsites/countries;${VAL}/$1" | cat - | grep site-listing | cut -d ">" -f 7 | cut -d "<" -f 1)
+		for DOMAIN in $(wget -qO- "http://www.alexa.com/topsites/countries;${VAL}/$1" | cat - | grep site-listing | cut -d ">" -f 7 | cut -d "<" -f 1)
 		do
-			digSite $SITE
+			DOMAINLVL=$(echo $DOMAIN | sed -e 's/\.$//' | awk -F . '{ print NF }')
+			digSite $DOMAIN
 		done
 	done
 }
@@ -54,7 +56,9 @@ usage()
 	echo "-h              Display the help and exit"
 	echo "-i              Interactive mode"
 	echo "-p              Use proxychains to safely query name servers"
-	echo "-z              Save the zone transfer in the wd in this form: domain_axfr.log" 
+	echo "-q 							Quiet mode when using proxychains (all proxychains' output is discarded)"
+	echo "-r							Test recursively every subdomain of a vulnerable domain"
+	echo "-z              Save the zone transfer in the wd in the following form: domain_axfr.log" 
 }
 
 iMode()
@@ -74,42 +78,107 @@ iMode()
 	echo "# GNU General Public License for more details."
 	echo -e "\n\n"
 	echo "Insert the domain you want to test (Ctrl+d to terminate):"
-	while read SITE
+	while read DOMAIN
 	do
-		digSite $SITE
+		DOMAINLVL=$(echo $DOMAIN | sed -e 's/\.$//' | awk -F . '{ print NF }')
+		digSite $DOMAIN
 		echo "Insert the domain you want to test (Ctrl+d to terminate):"
 	done
 }
 
+drawTree()
+{
+	unset TREE1 TREE2
+	TREE1="|--"
+	TREE2="|  "
+	if [ "$DOMAIN" = "$1" ]
+	then
+		[ -n "$VULNERABLE" ] && printf "DOMAIN $1:$VULNERABLE ${GREEN}VULNERABLE!${RCOLOR}\n"
+		[ -n "$NOT_VULNERABLE" ] && printf "DOMAIN $1:$NOT_VULNERABLE ${RED}NOT VULNERABLE!${RCOLOR}\n"
+		return 
+ 	fi
+	LVLDIFF=$(( $(echo $1 | sed -e 's/\.$//' | awk -F . '{ print NF }') - $DOMAINLVL))
+	if [ $LVLDIFF -eq 1 ]
+	then
+		[ -n "$VULNERABLE" ] && printf "${TREE1}DOMAIN $1:$VULNERABLE ${GREEN}VULNERABLE!${RCOLOR}\n"
+		if [ ! -n "$VULNERABLE" ]
+		then
+			printf "${TREE1}DOMAIN $1:$NOT_VULNERABLE ${RED}NOT VULNERABLE!${RCOLOR}\n"
+		else
+			[ -n "$NOT_VULNERABLE" ] && printf "${TREE2}DOMAIN $1:$NOT_VULNERABLE ${RED}NOT VULNERABLE!${RCOLOR}\n"
+		fi
+	else
+		for i in $(seq 1 $LVLDIFF)
+		do
+			TREE1=" $TREE1"
+			TREE2=" $TREE2"
+		done
+		[ -n "$VULNERABLE" ] && printf "|${TREE1}DOMAIN $1:$VULNERABLE ${GREEN}VULNERABLE!${RCOLOR}\n"
+		if [ ! -n "$VULNERABLE" ]
+		then
+			printf "|${TREE1}DOMAIN $1:$NOT_VULNERABLE ${RED}NOT VULNERABLE!${RCOLOR}\n"
+		else
+			[ -n "$NOT_VULNERABLE" ] && printf "|${TREE2}DOMAIN $1:$NOT_VULNERABLE ${RED}NOT VULNERABLE!${RCOLOR}\n"
+		fi
+	fi
+}
+
 digSite()
 {
+	unset VULNERABLE NOT_VULNERABLE
 	#$1 domain to test
 	FILE="${1}_axfr.log"
-	NS="$(dig $1 ns | egrep "^$1" | awk '{ print $5 }')"
+	NS="$($QUIET1 $PROXY dig $1 ns $QUIET2 | egrep "^$1" | awk '{ print $5 }')"
 	for NSERVER in $(echo $NS)
 	do
-		if $PROXY dig @$NSERVER $1 axfr | egrep '[[:space:]]NS[[:space:]]' > /dev/null 2>&1
+		if [ "$ZONETRAN"  = 'enabled' -a ! -f $FILE ]
 		then
-			VULNERABLE="$VULNERABLE $NSERVER"
-			[ "$ZONETRAN"  = 'enabled' -a ! -f $FILE ] && $PROXY dig @$NSERVER $1 axfr > $FILE
+			if $QUIET1 $PROXY dig @$NSERVER $1 axfr $QUIET2 | tee /tmp/$FILE | egrep '[[:space:]]NS[[:space:]]' > /dev/null 2>&1
+			then
+				mv /tmp/$FILE .
+				VULNERABLE="$VULNERABLE $NSERVER"
+			else
+				rm /tmp/$FILE
+				NOT_VULNERABLE="$NOT_VULNERABLE $NSERVER"
+			fi
 		else
-			NOT_VULNERABLE="$NOT_VULNERABLE $NSERVER"
+			if $QUIET1 $PROXY dig @$NSERVER $1 axfr $QUIET2 | egrep '[[:space:]]NS[[:space:]]' > /dev/null 2>&1
+			then
+				VULNERABLE="$VULNERABLE $NSERVER"
+			else
+				NOT_VULNERABLE="$NOT_VULNERABLE $NSERVER"
+			fi
 		fi
 	done
-	[ -n "$VULNERABLE" ] && echo "DOMAIN $1:$VULNERABLE VULNERABLE!"
-	[ -n "$NOT_VULNERABLE" ] && echo "DOMAIN $1:$NOT_VULNERABLE NOT VULNERABLE!"
-	unset VULNERABLE NOT_VULNERABLE
+	[ -n "$VULNERABLE" -o -n "$NOT_VULNERABLE" ] && drawTree $1
+	if [ "$RECURSIVE" = 'enabled' -a -n "$VULNERABLE" ]
+	then
+		if [ -f $FILE ]
+		then
+			for SDOMAIN in $(egrep '[[:space:]]NS[[:space:]]' $FILE | egrep -vi "^$1" | awk '{ print $1 }' | sort -u)
+			do
+				digSite $SDOMAIN
+			done
+		else
+			for SDOMAIN in $($QUIET1 $PROXY dig @$(echo $VULNERABLE | awk '{ print $1 }') $1 axfr $QUIET2 | egrep '[[:space:]]NS[[:space:]]' | egrep -vi "^$1" | awk '{ print $1 }' | sort -u)	
+			do
+				digSite $SDOMAIN
+			done
+		fi
+	fi
 }
 
 parse()
 {
-	while getopts ':c:hipz' OPTION
+	while getopts ':c:hipqrz' OPTION
 	do
 		case $OPTION in
 		c)ALEXA500='enabled'; COUNTRY="$OPTARG";;
 		h)usage && exit 0;;
 		i)IMODE='enabled';;
-		p)[ ! -f /usr/bin/proxychains ] && echo "Proxychains is not installed...exiting" && exit 3 || PROXY='proxychains';;
+		p)[ ! -x $(which proxychains) ] && echo "Proxychains is not installed...exiting" && exit 3 || PROXY='proxychains';;
+		q)QUIET1='eval'; QUIET2='2>/dev/null';;
+		r)RECURSIVE='enabled';;
 		z)ZONETRAN='enabled';;
 		\?)
 			echo "Option not reconized...exiting"
@@ -124,15 +193,24 @@ parse()
 	[ "$ALEXA500" = 'enabled' ] && alexaTop500 $COUNTRY && exit 0
 	[ "$IMODE" = 'enabled' ] && iMode && exit 0
 
+	#No arguments
+	[ $# -eq 0 ] && usage && exit 0
+
+	#Every site specified as argument is tested
 	for CONT in $(seq 1 $#)
 	do
-	digSite ${!CONT}
+	DOMAIN=${!CONT}
+	DOMAINLVL=$(echo $DOMAIN | sed -e 's/\.$//' | awk -F . '{ print NF }')
+	digSite $DOMAIN
 	done
 }
 
 #############
 #SCRIPT START
 #############
+GREEN='\033[1;92m'
+RED='\033[1;91m'
+RCOLOR='\033[1;00m'
 
 parse "$@"
 exit 0
